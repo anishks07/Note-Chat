687b76601a877e2f0e8e44b3122e6fdf804b1a81
Anish KT <anishkst07@gmail.com>
2025-07-12 13:23:14 +0530

    env setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Anish Konda",
+    author_email="anishkst25@gmail.com",
+    packages= find_packages(),
+    install_requires=[])
